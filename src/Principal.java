d78ce261ee851b833233f3be13c40db1a9d8528f
fabionc <fabionc@unicamp.br>
2024-12-02 10:22:18 -0300

    Create first ArrayList

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Ola");
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("C++");
+        list.add("GO");
+
+        for(String elemento : list){
+            System.out.println(elemento);
+        }
     }
 }
